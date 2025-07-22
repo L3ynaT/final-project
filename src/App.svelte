7767ed444156ee3d0a9b80c6df7d1d455b2703f9
L3ynaT <leynatran1221@gmail.com>
2025-07-21 17:36:48 -0700

    deleted unused files

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,11 @@
 <script>
-  import LeftAlignedExample from "./sections/LeftAlignedExample.svelte";
   import HPSA from "./sections/HPSA.svelte";
-  import ImageExample from "./sections/ImageExample.svelte";
   import TitleSection from "./sections/TitleSection.svelte";
-  import InteresectionObserverBasicExample from "./sections/InteresectionObserverBasicExample.svelte";
-  import IntersectionObserverAlertExample from "./sections/IntersectionObserverAlertExample.svelte";
-  import DisappearingDuckExample from "./sections/DisappearingDuckExample.svelte";
-  import IntersectionObserverSectionCard from "./sections/IntersectionObserverSectionCard.svelte";
-  import IntersectionObserverListExample from "./sections/IntersectionObserverListExample.svelte";
-
   import StandAlone from "./sections/StandAlone.svelte";
   import CountyTitleSection from "./sections/CountyTitleSection.svelte";
   import BarChartScroll from "./sections/BarChartScroll.svelte";
   import HealthInsurance from "./sections/HealthInsurance.svelte";
   import Conclusion from "./sections/Conclusion.svelte";
-  
 </script>
 
 <main>
